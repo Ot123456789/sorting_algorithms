b981dddf63916e28e5a7e1cd433f4c231c9906e3
Ot123456789 <otmaneelouafi@gmail.com>
2024-02-20 16:10:52 -0800

    up

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -14,10 +14,11 @@
 */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
+
 
 /* print */
 void print_array(const int *array, size_t size);
@@ -31,10 +32,9 @@ void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
-void counting_sort(int array, size_t size);
+void counting_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
-
