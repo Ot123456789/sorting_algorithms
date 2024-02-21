31114d14ea928ea55f2479ed47ce828db1592f30
Ot123456789 <otmaneelouafi@gmail.com>
2024-02-20 16:06:12 -0800

    up

--- sorting_algorithms/0-bubble_sort.c
+++ sorting_algorithms/0-bubble_sort.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * bubble_sort - Function that sorts an array of ints
  * using bubble sort algorithm
@@ -10,25 +9,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-    size_t i, k;
-
-    if (!array || !size)
-        return;
-
-    i = 0;
-    while (i < size)
-    {
-        for (k = 0; k < size - 1; k++)
-        {
-            if (array[k] > array[k + 1])
-            {
-                temp = array[k];
-                array[k] = array[k + 1];
-                array[k + 1] = temp;
-                print_array(array, size);
-            }
-        }
-        i++;
-    }
+	int temp;
+	size_t i, k;
+	
+	if (!array || !size)
+		return;
+	
+	i = 0;
+	while (i < size)
+	{
+		for (k = 0; k < size -1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
